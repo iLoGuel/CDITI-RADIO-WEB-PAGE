4e612c9d09db78401a78aff5afb4998e7475548c
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-11 12:29:40 -0500

    First commit and v1.0.0

---
+++ src/routes/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+  import logo from "$lib/images/cditi-radio-logo.webp";
+
+  // Función para mostrar el mensaje "Próximamente :D"
+  function mostrarMensaje() {
+    alert("La pagina está en desarrollo actualmente y estará disponible próximamente. ¡Gracias por tu paciencia! 😊");
+  }
+</script>
+
+<header>
+  <nav class="max-w-7xl m-auto w-auto py-4 px-5 flex justify-between items-center">
+    <a href="/">
+      <img src={logo} alt="" class="h-14 p-1 hover:p-0 transition-all hover:drop-shadow-lg">
+    </a>
+    <ul class="hidden sm:flex gap-5">
+      <li><a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="/">Inicio</a></li>
+      <li><a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="/">Blog</a></li>
+      <li><a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="/" on:click={mostrarMensaje}>Podcasts</a></li>
+      <li><a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="/" on:click={mostrarMensaje}>Contacto</a></li>
+    </ul>
+  </nav>
+</header>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#6cbc6e',
+        'secondary': '#c9e9c9'
+      }
+    },
+  },
+  plugins: [],
+}
+
