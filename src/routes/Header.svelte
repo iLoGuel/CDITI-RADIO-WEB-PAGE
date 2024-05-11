74ee66f5fc141ff7dd957f6619cf834ac3646b3e
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-11 14:43:41 -0500

    Add scale to the button

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -12,7 +12,7 @@
     <a href="/">
       <img src={logo} alt="" class="h-14 hover:scale-105 transition-all hover:drop-shadow-lg">
     </a>
-    <button class="sm:hidden hover:shadow p-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer flex"><i class="fa-solid fa-bars"></i></button>
+    <button class="sm:hidden hover:scale-105 hover:shadow p-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer flex"><i class="fa-solid fa-bars"></i></button>
     <ul class="hidden sm:flex gap-5">
       <li class="hover:scale-105"><a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="/">Inicio</a></li>
       <li class="hover:scale-105"><a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="https://senarisaraldadosquebradas.blogspot.com/" target="_blank">Blog</a></li>
