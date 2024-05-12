2054fc24b8d4760e48c01004745618e2e44f6183
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-05-12 13:45:09 -0500

    Update footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   import "./styles.css";
 
   import logo from "$lib/images/cditi-radio-logo.webp";
+  import logoSennova from "$lib/images/sennova-logo.png";
+  import logoSena from "$lib/images/sena-logo.svg";
 
   // Función para mostrar el mensaje "Próximamente :D"
   function mostrarMensaje() {
@@ -19,7 +21,7 @@
       <a href="/">
         <img
           src={logo}
-          alt=""
+          alt="Logo de CDITI RADIO"
           class="h-14 hover:scale-105 transition-all hover:drop-shadow-lg"
         />
       </a>
@@ -60,14 +62,25 @@
   </header>
 
   <slot />
-
-  <footer>
-    <p class="text-center text-balance my-5">
-      Diseñado y desarrollado por la <span class="font-bold text-primary"
-        >Fábrica de Software</span
-      >
-      de
-      <span class="font-bold text-primary">CDITI SENA Dosquebradas</span>
-    </p>
+  <footer class="bg-primary py-10 text-white">
+    <div class="max-w-7xl px-5 mx-auto flex gap-5 justify-center lg:justify-between items-center flex-wrap">
+      <p class="text-sm">
+        Servicio Nacional de Aprendizaje SENA-CENTRO DE DISEÑO E INNOVACIÓN
+        TECNOLOGÍCA INDUSTRIAL-Regional Risaralda<br />
+        Dirección: Transv. 7 Calle 26 Barrio Santa Isabel Dosquebradas - +57 (6)
+        3113700 IP 63361<br />
+        Conmutador Nacional (57 1) 5461500 <br /> Atención telefónica: lunes a
+        viernes 8:00 a.m. a 6:00 p.m. <br />
+        Atención al ciudadano: Bogotá (57 1) 3430111 - Línea gratuita y resto del
+        país 018000 910270 <br />
+        Atención al empresario: Bogotá (57 1) 3430101 - Línea gratuita y resto del
+        país 018000 910682
+      </p>
+      <div class="flex bg-white justify-between rounded-xl p-4 w-fit h-fit gap-3">
+        <img class="h-14" src={logoSena} alt="Logo de SENA" />
+        <hr class="bg-primary h-auto rounded-xl" style="width: 2px;">
+        <img class="h-14" src={logoSennova} alt="" />
+      </div>
+    </div>
   </footer>
 </div>
