89c96fd5125f0c51dbe279c7b5ca5db96be0b7a2
Miguel Ángel López Laverde <94725688+iLoGuel@users.noreply.github.com>
2024-05-12 19:15:27 -0500

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -20,7 +20,7 @@
     </a>
     <button class="sm:hidden hover:scale-105 hover:shadow p-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer flex" on:click={toggleMenu}><i class="fa-solid fa-bars"></i></button>
     <div class={`transition-opacity menu ${isActive? 'active' : ''}`} on:click={toggleMenu}>
-      <ul class={`transition-all menu__container ${isActive? 'active' : ''}`}>
+      <ul class={`transition-transform menu__container ${isActive? 'active' : ''}`}>
         <li class="hover:scale-105">
           <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary transition-all rounded-xl cursor-pointer no-underline" href="/">Inicio</a>
         </li>
