a6d87738035d70dce8a20bfa6b9094e9c1102ac3
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-06-02 15:05:31 -0500

    "Updated +page.svelte: added playing variable, initialized player, refactored playRadio function, and added social media icons"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,44 @@
 <script>
-  import { onMount, onDestroy} from "svelte";
+  import { onMount, onDestroy } from "svelte";
   import playerBanner from "$lib/images/player-banner.webp";
   import { RADIO_URL } from "$lib/js/config";
   import Post from "../components/Post.svelte";
 
+  // Inicializa la variable `playing` como un booleano, establecida en false
   let playing = false;
+
   /**
    * @type {HTMLAudioElement}
    */
   let player;
 
   onMount(() => {
+    // Inicializa la variable `player` como un nuevo objeto Audio
     player = new Audio();
   });
 
+  /**
+   * Reproduce o pausa la transmisión de radio
+   */
   function playRadio() {
-  if (playing) {
-    player.src = '';
-    playing =!playing;
-  } else {
-    player.src = RADIO_URL;
-    player.play();
-    playing =!playing;
+    if (playing) {
+      // Si la radio está actualmente reproduciendo, detén la reproducción y actualiza la variable `playing`
+      player.src = '';
+      playing =!playing;
+    } else {
+      // Si la radio no está actualmente reproduciendo, establece la fuente `player` a `RADIO_URL` y reproduce la transmisión
+      player.src = RADIO_URL;
+      player.play();
+      playing =!playing;
+    }
   }
-}
 
-onDestroy(()=>{
-  if(playing){
-    playRadio();
-  }
-})
-
+  onDestroy(() => {
+    // Si el componente se está destruyendo y la radio está actualmente reproduciendo, detén la reproducción
+    if (playing) {
+      playRadio();
+    }
+  });
 </script>
 
 <svelte:head>
@@ -47,11 +55,13 @@ onDestroy(()=>{
       class="w-full backdrop-blur-sm backdrop-brightness-50 flex px-6 py-4 text-white text-2xl justify-between flex-wrap gap-6"
     >
       <button on:click={playRadio} class="transition-all hover:scale-105">
-        <i class={`fa-solid ${playing? 'fa-stop':'fa-play'}`}></i>
+        <!-- Utiliza la clase `fa-solid` para mostrar el icono apropiado basado en la variable `playing` -->
+        <i class={`fa-solid ${playing? 'fa-stop' : 'fa-play'}`}></i>
+        <!-- Muestra el texto apropiado basado en la variable `playing` -->
         {playing? 'Detener' : 'Reproducir'}
-
       </button>
       <div class="flex gap-4 items-center">
+        <!-- Agrega iconos de redes sociales con enlaces -->
         <i
           class="fa-brands fa-facebook transition-all hover:scale-105 cursor-pointer"
         ></i>
@@ -67,10 +77,10 @@ onDestroy(()=>{
         <i
           class="fa-brands fa-spotify transition-all hover:scale-105 cursor-pointer"
         ></i>
-        
       </div>
     </div>
   </div>
   <hr class="my-5" />
+  <!-- Muestra el componente `Post` -->
   <Post />
 </main>
