c9b873dcd7e41f4922a328fd377305b22c961a81
Miguel Ángel López L. <malopezl0512@gmail.com>
2024-06-05 11:42:00 -0500

    "Updated Header component: refactored menu layout, styles, and toggle behavior"

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,6 +4,7 @@
 
   function mostrarMensaje() {
     alert("¡Oops Estamos trabajando en esta función actualmente y estará disponible muy pronto. Agradecemos tu comprensión y paciencia mientras la preparamos para ti. ¡Gracias por visitarnos!");
+    isActive ? toggleMenu() : '';
   }
 
   function toggleMenu() {
@@ -24,62 +25,22 @@
     </a>
     <button class="sm:hidden hover:scale-105 hover:shadow p-4 font-bold hover:bg-secondary hover:text-primary transition-transform rounded-xl flex" on:click={toggleMenu}><i class="fa-solid fa-bars"></i></button>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class={`menu ${isActive? 'active' : ''}`} on:click={toggleMenu}>
-      <ul class={`transition menu__container ${isActive? 'active' : ''}`}>
-        <li class="transition-transform hover:scale-105 cursor-pointer">
-          <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="/">Inicio</a>
-        </li>
-        <li class="transition-transform hover:scale-105 cursor-pointer">
-          <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="https://senarisaraldadosquebradas.blogspot.com/" target="_blank">Blog</a>
-        </li>
-        <li class="transition-transform hover:scale-105 cursor-pointer">
-          <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="/" on:click={mostrarMensaje}>Podcasts</a>
-        </li>
-        <li class="transition-transform hover:scale-105 cursor-pointer">
-          <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="/contacto">Contacto</a>
-        </li>
-      </ul>
+    <div class="{`transition-all w-screen sm:h-auto fixed top-0 left-0 bg-black opacity-50 z-40 ${isActive? ' flex h-screen':'h-auto'}`}" on:click={toggleMenu}>
     </div>
+    <ul class={`transition-all fixed sm:static flex flex-col sm:flex-row bg-white rounded-l-xl z-50 sm:translate-x-0 w-80 sm:w-auto  h-screen sm:h-auto sm:bg-transparent top-0 right-0 p-4 gap-4 ${isActive? 'flex' : 'translate-x-full'}`}>
+      <li class="transition-transform hover:scale-105 cursor-pointer">
+        <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="/" on:click={isActive ? toggleMenu : ''}>Inicio</a>
+      </li>
+      <li class="transition-transform hover:scale-105 cursor-pointer">
+        <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="https://senarisaraldadosquebradas.blogspot.com/" target="_blank" on:click={isActive ? toggleMenu : ''}>Blog</a>
+      </li>
+      <li class="transition-transform hover:scale-105 cursor-pointer">
+        <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="/" on:click={mostrarMensaje} >Podcasts</a>
+      </li>
+      <li class="transition-transform hover:scale-105 cursor-pointer">
+        <a class="hover:shadow py-2 px-4 font-bold hover:bg-secondary hover:text-primary rounded-xl" href="/contacto" on:click={isActive ? toggleMenu : ''}>Contacto</a>
+      </li>
+    </ul>
   </nav>
 </header>
 
-<style>
- .menu__container {
-    display: flex;
-    flex-direction: row;
-  }
-
-  @media (max-width: 640px) {
-   .menu {
-      width: 100vw;
-      height: 100vh;
-      position: fixed;
-      background-color: transparent;
-      top: 0;
-      left: 0;
-      z-index: -1;
-    }
-
-   .menu.active {
-      background-color: rgba(0, 0, 0, 0.700);
-      z-index: 0;
-    }
-
-   .menu__container {
-      transform: translateX(100%);
-      display: flex;
-      flex-direction: column;
-      padding: 1.25rem;
-      gap: 1.25rem;
-      background-color: white;
-      margin-left: auto;
-      width: 60vw;
-      height: 100vh;
-      border-radius: 12px 0 0 12px;
-    }
-
-   .menu__container.active {
-      transform: translateX(0);
-    }
-  }
-</style>
