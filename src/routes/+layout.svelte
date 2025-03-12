d9ffdbfe9d1e844a0c7b9f1f27e9892705f3f0c0
Miguel Ángel López L. <malopezl0512@gmail.com>
2025-03-12 16:19:29 -0500

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,8 @@
   import "./styles.css";
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
-  import Whatsapp from "../components/Whatsapp.svelte";
 </script>
 
 <Header />
 <slot />
 <Footer />
-<Whatsapp/>
